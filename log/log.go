2e59f620ad7754b878e5caf72e5bee67f8694d27
hayson <hayson97@163.com>
2025-06-24 21:05:03 +0800

    adjust slog config

--- log/log.go
+++ log/log.go
@@ -6,11 +6,11 @@ import (
 )
 
 type Config struct {
-	Level        string `yaml:"level"`         // 日志级别，支持 debug info warn error 默认info
-	Filename     string `yaml:"filename"`      // 日志文件名，若文件名为空，则会将日志打印至stdout
-	MaxAge       int    `yaml:"max_age"`       // 日志文件最大保存天数，默认为30
-	ConsoleFmt   bool   `yaml:"console_fmt"`   // 日志默认以json方式打印，若ConsoleFmt为true，将以更适合终端阅读的方式打印，此方式性能很差
-	ConsoleColor bool   `yaml:"console_color"` // 日志采用终端打印格式时是否包含颜色
+	Level        string `mapstructure:"level"`         // 日志级别，支持 debug info warn error 默认info
+	Filename     string `mapstructure:"filename"`      // 日志文件名，若文件名为空，则会将日志打印至stdout
+	MaxAge       int    `mapstructure:"max_age"`       // 日志文件最大保存天数，默认为30
+	ConsoleFmt   bool   `mapstructure:"console_fmt"`   // 日志默认以json方式打印，若ConsoleFmt为true，将以更适合终端阅读的方式打印，此方式性能很差
+	ConsoleColor bool   `mapstructure:"console_color"` // 日志采用终端打印格式时是否包含颜色
 }
 
 // 默认将日志打印至stdout，使用终端格式打印，且包含颜色，开发体验更好，但性能较差，不适于线上使用
